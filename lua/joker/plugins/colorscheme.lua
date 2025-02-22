3e7d226a082f1a8886836b03ee0477d38f2c6c87
joker <joker@MacBookPro.attlocal.net>
2025-02-22 10:48:28 -0800

    edit keymappings for several plugins

--- lua/joker/plugins/colorscheme.lua
+++ lua/joker/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "auto", -- latte, frappe, macchiato, mocha
-                background = { -- :h background
-                    light = "frappe",
-                    dark = "frappe",
-                }
-            })
-            
-            vim.cmd([[colorscheme catppuccin]])
-        end,
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "auto", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "frappe",
+				},
+			})
+
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
